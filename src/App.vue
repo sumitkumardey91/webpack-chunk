0235d38d0b73c2ac49a943d603d58b2025d1dbb3
sumitkumardey91 <sumit.kumar@mobiotics.com>
2019-09-19 18:52:59 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <component :is="msg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.msg = () => import(/* webpackChunkName: 'welcomeHere' */  './components/HelloWorld.vue')
+    }, 2000)
+  }
+}
+</script>
+
+
